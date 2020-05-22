ba35bdad81e8faeed380e9eaf992806b22e278ee
Ianadika44 <adika19ian@gmail.com>
2020-05-22 15:29:30 +0300

    added the test save method to taste our app

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Editor,Article,tags
 
 # Create your tests here.
+class EditorTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.ian= Editor(first_name = 'ian', last_name ='adika', email ='adika19ian@gmail.com')
+        
+        # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.ian,Editor))
+        
+       # Testing Save Method
+    def test_save_method(self):
+        self.ian.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > 0)
