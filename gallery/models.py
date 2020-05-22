ac78bf2c852c835154f8c31b35f3cdc935a2f8a8
Ianadika44 <adika19ian@gmail.com>
2020-05-22 15:08:25 +0300

    returned a string on the models that will return a string representation of our model

--- gallery/models.py
+++ gallery/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-class Editor(models.Model):
+cclass Editor(models.Model):
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
     email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
