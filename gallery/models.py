7a38b0e9fe35a3213a668dd20230db6bde326a24
Ianadika44 <adika19ian@gmail.com>
2020-05-22 16:03:21 +0300

    created a class method that takes date object as an argument

--- gallery/models.py
+++ gallery/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
 class Editor(models.Model):
@@ -27,3 +28,14 @@ class Article(models.Model):
     editor = models.ForeignKey(Editor)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
+    
+    @classmethod
+    def todays_gallery(cls):
+        today = dt.date.today()
+        gallery = cls.objects.filter(pub_date__date = today)
+        return gallery
+    
+    @classmethod
+    def days_gallery(cls,date):
+        gallery = cls.objects.filter(pub_date__date = date)
+        return gallery
--- gallery/tests.py
+++ gallery/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import Editor,Article,tags
+import datetime as dt
 
 # Create your tests here.
 class EditorTestClass(TestCase):
@@ -42,3 +43,9 @@ class ArticleTestClass(TestCase):
     def test_get_gallery_today(self):
         today_gallery = Article.todays_gallery()
         self.assertTrue(len(today_gallery)>0)
+    
+    def test_get_gallery_by_date(self):
+        test_date = '2017-03-17'
+        date = dt.datetime.strptime(test_date, '%Y-%m-%d').date()
+        gallery_by_date = Article.days_gallery(date)
+        self.assertTrue(len(gallery_by_date) == 0)
