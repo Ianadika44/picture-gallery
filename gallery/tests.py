b2981568e673a7ce32e8e2f936c544664de6ec98
Ianadika44 <adika19ian@gmail.com>
2020-05-22 15:49:47 +0300

    defined the two methods

--- gallery/tests.py
+++ gallery/tests.py
@@ -17,3 +17,24 @@ class EditorTestClass(TestCase):
         self.ian.save_editor()
         editors = Editor.objects.all()
         self.assertTrue(len(editors) > 0)
+        
+class ArticleTestClass(TestCase):
+
+    def setUp(self):
+        # Creating a new editor and saving it
+        self.ian= Editor(first_name = 'ian', last_name ='adika', email ='adika19ian@gmail.com')
+        self.ian.save_editor()
+
+        # Creating a new tag and saving it
+        self.new_tag = tags(name = 'testing')
+        self.new_tag.save()
+
+        self.new_article= Article(title = 'Test Article',post = 'This is a random test Post',editor = self.ian)
+        self.new_article.save()
+
+        self.new_article.tags.add(self.new_tag)
+
+    def tearDown(self):
+        Editor.objects.all().delete()
+        tags.objects.all().delete()
+        Article.objects.all().delete()
