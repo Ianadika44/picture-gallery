db8d5ae40bcd375d608993d57290199bdc78a97d
Ianadika44 <adika19ian@gmail.com>
2020-05-23 11:17:21 +0300

    added the media articles

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
-from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
+from . import views
+
 
 urlpatterns=[
-    # url('^$',views.welcome,name = 'welcome'),
     url(r'^article/(\d+)',views.article,name ='article'),
     url('^today/$',views.gallery_today,name='galleryToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_gallery,name = 'pastGallery'),
